37f54991b6dad3b25bba67fa0bf33bcb2988d5f3
Vince Sparks <vsparks@TCSDOMAIN.local>
2018-02-02 17:00:38 -0600

    fix: added logic to account for books without authors or cover URLs

--- src/Book.js
+++ src/Book.js
@@ -14,14 +14,17 @@ class Book extends Component {
     return (
       <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.thumbnail + '")' }}></div>
+        {(book.imageLinks) ?
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.thumbnail + '")' }}></div> :
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundColor: 'black' }}></div> 
+        }
         <BookshelfChanger
           book={book}
           onMoveBook={onMoveBook}
         />
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors.join(', ')}</div>
+        <div className="book-authors">{book.author ? book.authors.join(', ') : ''}</div>
       </div>
     )
   }
