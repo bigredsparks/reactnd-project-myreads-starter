ad6820ede161e87d25af3b35867c7149fae78ca3
Vince Sparks <svsparks@swbell.net>
2018-02-06 20:56:06 -0600

    refactor: added check to make sure both imageLinks and thumbnail are valid before displaying book image

--- src/Book.js
+++ src/Book.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import BookshelfChanger from './BookshelfChanger'
 
+// if no image link or thumbnail is defined for book then display a black rectangle
 const Book = (props) => (
   <div className="book">
   <div className="book-top">
-    {(props.book.imageLinks) ?
+    {(props.book.imageLinks && props.book.imageLinks.thumbnail) ?
       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + props.book.imageLinks.thumbnail + '")' }}></div> :
-      <div className="book-cover" style={{ width: 128, height: 193, backgroundColor: 'black' }}></div> 
+      <div className="book-cover" style={{ width: 128, height: 193, backgroundColor: 'black' }}></div>
     }
     <BookshelfChanger
       book={props.book}
